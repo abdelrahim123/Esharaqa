f3ba1f21c36b8947d37b16f2e8571f5124b91169
Abdelrahim_Aref <40636672+BEdoAref@users.noreply.github.com>
2019-05-03 08:45:41 +0300

    commit

--- js/e.js
+++ js/e.js
@@ -18,7 +18,7 @@ var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
   modal.style.display = "block";
-  modalImg.src = "img/people.jpg";
+  modalImg.src = "img/206706-OZT2BG-158.png";
   captionText.innerHTML = Eshraqa;
 }
 
